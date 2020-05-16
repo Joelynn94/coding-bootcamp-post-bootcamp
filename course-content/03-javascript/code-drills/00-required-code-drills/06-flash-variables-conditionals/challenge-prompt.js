9b2470b5f60b6219dbc5eb17a15e5d814993bc0c
Joseph Lynn <josephlynn94@gmail.com>
2020-05-16 07:34:54 -0400

    finished JS arrays with conditionals challenge

--- course-content/03-javascript/code-drills/00-required-code-drills/06-flash-variables-conditionals/challenge-prompt.js
+++ course-content/03-javascript/code-drills/00-required-code-drills/06-flash-variables-conditionals/challenge-prompt.js
@@ -1,73 +1,89 @@
 // declare a variable named farm
-
-
+let farm;
 
 // set that farm variable to be an array with the elements
 // Pig, Cow, Chicken, Dog, Horse, Sheep
-
-
+farm = ['Pig', 'Cow', 'Chicken', 'Dog', 'Horse', 'Sheep'];
 
 // write an if statement that creates an alert that says "RWAR!" if the first element of farm is not "godzilla"
-
-
+if (farm[0] !== 'Godzilla') {
+  alert('RWAR!');
+}
 
 // write another if statement that creates an alert "SCREECH!" if the last element of farm is "mothra"
-
-
+if (farm[farm.length - 1] === 'Mothra') {
+  alert('SCREECH!');
+}
 
 // declare a variable named dog with a value of "Spot"
-
+const dog = 'Spot';
 
 // declare 3 variables cat, car, city without assigning them values
-
+let cat, car, city;
 
 // assign the string "Farley" to cat
-
+cat = 'Farley';
 
 // assign the string "San Francisco" to city
-
+city = 'San Francisco';
 
 // assign the string "Prius" to car
+car = 'Prius';
 
-
-// using string concatenation, console log out the sentence 
+// using string concatenation, console log out the sentence
 // "See Spot run!" without explicitly using "Spot" inside the string.
+console.log(`See ${dog} run!`);
 
-
-// using string concatenation, console log out the sentence 
-//"I drive Farley around San Francisco in my Prius" without explicityly 
+// using string concatenation, console log out the sentence
+//"I drive Farley around San Francisco in my Prius" without explicityly
 //using "Farley", "San Francisco", and "Prius"
 // Remember to use the variables we just declared!
-
+console.log(`I drive ${cat} around ${city} in my ${car}`);
 
 // declare a variable budget and assign it a value of 5000
-
+const budget = 5000;
 
 // declare a variable rentCost and assign it a value of 1500
-
+const rentCost = 1500;
 
 // declare a variable utilitiesCost and assign it a value of 150
-
+const utilitiesCost = 150;
 
 // declare a variable foodCost and assign it a value of 250
-
+const foodCost = 250;
 
 // declare a variable transportationCost and assign it a value of 350
-
+const transportationCost = 350;
 
 // declare a variable computerCost and assign it a value of 2000
-
+const computerCost = 2000;
 
 // write an if statement that checks whether the sum of all our costs is within the budget.
-// if it is within budget, console log out the total cost, otherwise, create an alert telling
-// us the sum of all the costs.
+if (
+  rentCost + utilitiesCost + foodCost + transportationCost + computerCost <=
+  budget
+) {
+  // if it is within budget, console log out the total cost,
+  console.log(
+    rentCost + utilitiesCost + foodCost + transportationCost + computerCost
+  );
+} else {
+  // otherwise, create an alert telling us the sum of all the costs.
+  alert(
+    rentCost + utilitiesCost + foodCost + transportationCost + computerCost
+  );
+}
 
-
-
-// write an if statement that checks whether the rentCost is larger than 
+// write an if statement that checks whether the rentCost is larger than
 // the sum of the utilitiesCost, foodCost, and transportationCost
 // and if computerCost is greater than the sum of rentCost and utilities cost
-// if both those statements are true, then create an alert that says "Computer costs are too high!"
-// else, create an alert that says "Rent is too high!"
-
-
+if (
+  rentCost > utilitiesCost + foodCost + transportationCost &&
+  computerCost > rentCost + utilitiesCost
+) {
+  // if both those statements are true, then create an alert that says "Computer costs are too high!"
+  alert('Computer costs are too high!');
+} else {
+  // else, create an alert that says "Rent is too high!"
+  alert('Rent is too high!');
+}
