2a8618ab31bd049d90d080ca6ece7aa948b0c8ad
Joseph Lynn <josephlynn94@gmail.com>
2020-05-16 07:07:18 -0400

    finished JS user-prompt1 challenge

--- course-content/03-javascript/code-drills/00-required-code-drills/03-core-js-user-input-and-dom-1/challenge-prompt.js
+++ course-content/03-javascript/code-drills/00-required-code-drills/03-core-js-user-input-and-dom-1/challenge-prompt.js
@@ -1,31 +1,24 @@
 // User Input and Writing to DOM 1
 
-
-
 // Declare a variable `userName`
-
+let userName;
 
 // Prompt the user for their name and store their response into `userName`
 // hint: use javascript's built in `prompt` method
-
-
+userName = prompt('What is you name?');
 
 // Create an alert welcoming the user to the "Sandwich Shop" using the name they just gave us
-
-
+alert(`Welcome ${userName} to the Sandwhich Shop`);
 
 // Using the `confirm` method, ask the user if they would like a sandwich
 // Store their response into a new `wantsSandwich` variable
-
-
+const wantsSandwhich = confirm(`${userName} would you like a sandwhich?`);
 
 // Check if the user wanted a sandwich
-
-
-    // If the user did want a sandwich, use the `document.write` method to write "Your sandwich will be ready soon!" to the DOM
-
-    
-// Else, say goodbye to the user using the name they gave us earlier
-
-
-
+if (wantsSandwhich) {
+  // If the user did want a sandwich, use the `document.write` method to write "Your sandwich will be ready soon!" to the DOM
+  document.write(`Your sandwich will be ready soon!`);
+} else {
+  // Else, say goodbye to the user using the name they gave us earlier
+  document.write(`Goodbye ${userName}`);
+}
