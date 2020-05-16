bc18779070eae0a2c1ee3a3778d7dd95835a33c1
Joseph Lynn <josephlynn94@gmail.com>
2020-05-16 07:00:30 -0400

    finished considtionals challenge

--- course-content/03-javascript/code-drills/00-required-code-drills/01-core-js-variables/challenge-prompt.js
+++ course-content/03-javascript/code-drills/00-required-code-drills/01-core-js-variables/challenge-prompt.js
@@ -18,93 +18,72 @@ meaning that any code commented out is ignored.
 
 // Create a variable named `subject` without giving it a value:
 // example: var hobby;
-
 let subject;
 
 // Assign a value of "Programmers" to the variable `subject`
 // example: hobby = "problem solving"
-
 subject = 'Programmers';
 
 // Create a variable `firstName` and assign it a value of an empty string.
 // example: var emptyString = "";
-
 let firstName = '';
 
 // Assign a value of "Ada" to the variable `firstName`
-
 firstName = 'Ada';
 
 // Create a variable `lastName` and assign it a value of a string "Lovelace".
-
 let lastName = 'LoveLace';
 
 // Create a variable `fullName` and assign it a value of `firstName` and `lastName` concatenated together with a space between them.
-
 const fullName = firstName + ' ' + lastName;
 
 // Create a variable `profession` and assign it a value of a string, "Computer Programmer".
-
 const profession = 'Computer Programmer';
 
 // Create a variable `knownFor` and assign it a value of a string, "first computer programmer".
-
 const knownFor = 'first computer programmer';
 
 // Create a variable `firstAlgorithm` and assign it a value of a string, "Analytical Engine".
-
 const firstAlgorithm = 'Analytical Engine';
 
 // Create a variable `cityLocation` and assign it a value of a string, "London";
-
 const cityLocation = 'London';
 
 // Create a variable `countryLocation` and assign it a value of a string, "England";
-
 const countryLocation = 'England';
 
 // Create a variable `nationality` and assign it a value of a string, "British";
-
 const nationality = 'British';
 
 //Topic: integers
 
 // Create a variable `birthYear` and assign it a value of the integer, 1815.
-
 const birthYear = 1815;
 
 // Create a variable `deathYear` and assign it a value of the integer, 1852.
-
 const deathYear = 1852;
 
 // Create a variable `ageAtPassing` and assign it to be the difference between `deathYear` and `birthYear`.
-
 const ageAtPassing = deathYear - birthYear;
 
 // Create a variable `yearOfPublish` and assign it a value of the integer, 1842.
-
 const yearOfPublish = 1842;
 
 // Create a variable `ageAtYearOfPublish` and assign it to be the difference between `yearOfPublish` and `birthYear`.
-
 const ageAtYearOfPublish = yearOfPublish - birthYear;
 
 //Topic: console log
 
 //console log: "First Name: Ada" by concatenating the `firstName` variable to the back of a string of "First Name: "
-
 console.log(`First Name: ${firstName}`);
 
 //console log: "Last Name: Lovelace" by concatenating the `lastName` variable to the back of a string of "Last Name: "
-
 console.log(`Last Name: ${lastName}`);
 
 //console log: "Profession: Computer Programmer" by concatenating the `profession` variable to the back of a string of "Profession: "
-
 console.log(`Profession: ${profession}`);
 
 //console log: "BirthYear: 1815" by concatenating the `birthYear` variable to the back of a string of "BirthYear: "
-
 console.log(`BirthYear: ${birthYear}`);
 
 //Topic: Concat Values
@@ -114,7 +93,6 @@ console.log(`BirthYear: ${birthYear}`);
 //
 // Use the `subject`, `fullName`, `nationality`, `profession`, and `birthYear` variables instead of hard coding in the full string
 // much like you did for the console logs and `fullName`
-
 const statementOne = `${subject}: ${fullName} is a ${nationality} ${profession} born in ${birthYear}`;
 
 // Create a variable `statementTwo` and assign it a value of a string:
@@ -122,7 +100,6 @@ const statementOne = `${subject}: ${fullName} is a ${nationality} ${profession} 
 //
 // Use the `knownFor` variable instead of hard coding in the full string
 // much like you did for the console logs and `fullName`
-
 const statementTwo = `She is commonly referred to as the ${knownFor}`;
 
 // Create a variable `statementThree` and assign it a value of a string:
@@ -130,7 +107,6 @@ const statementTwo = `She is commonly referred to as the ${knownFor}`;
 //
 // Use the `yearOfPublish`, `firstAlgorithm`, and `ageAtYearOfPublish` variables instead of hard coding in the full string
 // much like you did for the console logs and `fullName`
-
 const statementThree = `In ${yearOfPublish} she published the first Algorithm, the ${firstAlgorithm}, at the age of ${ageAtYearOfPublish}`;
 
 // Create a variable `statementFour` and assign it a value of a string:
@@ -138,13 +114,11 @@ const statementThree = `In ${yearOfPublish} she published the first Algorithm, t
 //
 // Use the `nationality`, `cityLocation`, `countryLocation`, and `ageAtPassing` variables instead of hard coding in the full string
 // much like you did for the console logs and `fullName`
-
 const statementFour = `She was a ${nationality} Citizen who lived in ${cityLocation}, ${countryLocation} until her passing in ${deathYear} at the age of ${ageAtPassing}`;
 
 //Topic: console.log()
 
 //console log statementOne
-
 console.log(statementOne);
 
 //console log statementTwo
